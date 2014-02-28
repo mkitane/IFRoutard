846b716e7bc77376421cb4449166a49f9bbe03cf
thomasescure <thomas.escure@insa-lyon.fr>
2014-02-28 15:52:28 +0100

    édition des liens UML classe Conseiller

--- src/metier/modele/Conseiller.java
+++ src/metier/modele/Conseiller.java
@@ -4,18 +4,34 @@
  */
 package metier.modele;
 
+import javax.persistence.Id;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.*;
-
+import javax.persistence.OneToMany;
+import javax.persistence.ManyToMany;
 /**
  *
  * @author Thomas
  */
+@Entity
 public class Conseiller {
     // attributs privés
-
-    private Pays paysSpecialisation;
+    @Id   
+    @Column(name = "ID_CONSEILLER")
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+    
+    
+    @OneToMany
+    private Pays paysSpecialisation;
+    
     private String mdp;
+    @ManyToMany
     private ArrayList<Client> listeClients = new ArrayList<Client>();
 
     public Conseiller() {
